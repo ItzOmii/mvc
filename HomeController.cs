1540c4cc9af8d369608a69204cb634519d1c9cf7
ItzOmii <88758816+ItzOmii@users.noreply.github.com>
2024-03-12 23:45:49 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Ass_2.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http; 
+
+
+
+namespace Ass_2.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Submit(string email)
+    {
+        Dictionary<string, string> parameters = new Dictionary<string, string>();
+        parameters.Add("email", email);
+        var _data = ExecuteProcedure("GetData", parameters);
+        return new JsonResult(_data);
+    }
+
+    public object ExecuteProcedure(string procedureName, Dictionary<string, string> parameters)
+    {
+        string connectionString = "Data Source=OMII-SAII\\SQLEXPRESS02;Initial Catalog=Self;Integrated Security=true;";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            using (SqlCommand data = new SqlCommand(procedureName, connection))
+            {
+                data.CommandType = CommandType.StoredProcedure;
+                foreach (KeyValuePair<string, string> parameter in parameters)
+                {
+                    data.Parameters.AddWithValue("@" + parameter.Key, parameter.Value);
+                }
+
+                using (SqlDataReader reader = data.ExecuteReader())
+                {
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(reader);
+                    
+                    // Convert DataTable to List<Dictionary<string, object>> for serialization
+                    var result = new List<Dictionary<string, object>>();
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        var rowData = new Dictionary<string, object>();
+                        foreach (DataColumn column in dataTable.Columns)
+                        {
+                            rowData.Add(column.ColumnName, row[column]);
+                        }
+                        result.Add(rowData);
+                    }
+                    return result;
+                }
+            }
+        }
+    }
+
+    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ site.js
@@ -0,0 +1,73 @@
+﻿
+$(document).ready(function(){
+    $("#error").hide();
+    $("#main").show();
+})
+
+
+$("#email").keyup(function(){
+    debugger;
+    var reg=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if(!reg.test($("#email").val()))
+   {
+    $("#error").show();
+    $("#submit").prop('disabled',true);
+   }
+   else{
+    $("#error").hide();
+    $("#submit").prop('disabled',false);
+   }
+
+})
+
+var currentIndex = 0;
+options=[]
+ans=[]
+$("#submit").click(function(){
+    var email=$("#email").val();
+    $.ajax({  
+        type: "POST",  
+        url: '/Home/Submit',  
+        data: {email:email},  
+        dataType: "json",
+        async: true,
+        success: function (_data) {  
+            $("#section").hide();
+            $("#main").show(); 
+            $("#next").prop("disabled",true);
+            displayQuestion(_data);
+            
+        },  
+        error: function () {  
+            alert("Error while inserting data");  
+        }  
+    }); 
+    
+})
+
+
+$("#next").click(function() {
+    options.push($("input[name='radio']:checked").next('label').text());
+    if (hasNextQuestion(_data)) {
+        currentIndex++;
+        displayQuestion(_data);
+    } else {
+       
+    }
+});
+
+function displayQuestion(_data) {
+    var _this = _data[currentIndex];
+    ans.push(_this.Answer);
+       $("input[type='question'][value='0']").next().text(''+_this.Question+'');
+       var control=_this.Options.split(",");
+       $("input[type='radio']").each(function(i) {
+           $(this).next('label').text(control[i]);
+       });
+       $("#next").prop("disabled",false);
+     
+}
+
+function hasNextQuestion(data) {
+    return currentIndex < data.length - 1;
+}
